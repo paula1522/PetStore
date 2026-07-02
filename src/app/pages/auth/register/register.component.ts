9c339c8224265310c236c36fb5bc957296a16107
Paulina <Paula06sepulveda@gmail.com>
2026-07-02 11:25:20 -0500

    feat: agregar validaciones con Reactive Form y mostrar/ocultar contraseña

--- src/app/pages/auth/register/register.component.ts
+++ src/app/pages/auth/register/register.component.ts
@@ -1,12 +1,58 @@
 import { Component } from '@angular/core';
+import { FormBuilder, Validators, AbstractControl, ValidationErrors, ReactiveFormsModule } from '@angular/forms';
+
+//Exportar validador de contraseña
+export function passwordMatchValidator(control: AbstractControl): ValidationErrors | null {
+  const password = control.get('password')?.value;
+  const confirmPassword = control.get('confirmPassword')?.value;
+  if (!password || !confirmPassword) {
+    return null;
+  }
+  return password === confirmPassword ? null : { 'passwordMismatch': true };
+};  
+
 
 @Component({
   selector: 'app-register',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './register.component.html',
   styleUrl: './register.component.scss'
 })
 export class RegisterComponent {
+   
+  constructor(private fb: FormBuilder) { }
+
+  RegisterForm = this.fb.group({
+    username: ['', Validators.required],
+    firstName: ['', Validators.required],
+    lastName: ['', Validators.required],
+    phone: ['', Validators.required],
+    email: ['', [Validators.required, Validators.email]],
+    password: ['', [Validators.required,
+                    Validators.minLength(6), 
+                    Validators.maxLength(20)]],
+    confirmPassword: ['', Validators.required]
     
+  },{validators: [passwordMatchValidator]});
+
+
+ // ocultar/ver contraseña
+  showPassword = false;
+
+  togglePassword() {
+    this.showPassword = !this.showPassword;
+  }
+
+  guardarRegistro() {
+  
+      if(this.RegisterForm.invalid){
+        this.RegisterForm.markAllAsTouched();
+        return;
+      }
+  
+      console.log(this.RegisterForm.value);
+  
+  }
+  
 }
